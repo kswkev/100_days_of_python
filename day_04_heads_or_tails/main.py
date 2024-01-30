8e35f113bc9565db72d0f7238fad435a8370faa5
kswkev <kswkev@hotmail.com>
2024-01-30 13:35:56 +0000

    Day 04 heads or tails added

---
+++ day_04_heads_or_tails/main.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+value = random.randint(0,1)
+
+if value == 1:
+  print("Heads")
+else:
+  print("Tails")
