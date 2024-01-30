7a8ccfd7da4803695faba05c14822c80086b246b
kswkev <kswkev@hotmail.com>
2024-01-30 12:43:23 +0000

    Day 08 functions added

---
+++ day_08_functions/main.py
@@ -0,0 +1,14 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet(name):
+    print(f"Hello {name}")
+    name = "Pauline"
+
+
+myname = "Kevin"
+greet(myname)
+print(f"Hello {myname}")
