a2b05d73e77305a94c898d9b5805562d8c7e4deb
kswkev <kswkev@hotmail.com>
2024-01-31 17:11:15 +0000

    Day 27 more tkinter widgets added added

--- day_27/main.py
+++ day_27/main.py
@@ -9,4 +9,24 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="My Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+# Properties can be change by
+# accessing them directly
+my_label["text"] = "some new text"
+# or thought the config method
+my_label.config(text="more new text")
+
+# Creating a button
+button = tkinter.Button(text="Click Me")
+button.pack()
+
+def button_clicked():
+    global my_label
+    global input
+    my_label["text"] = input.get()
+
+button["command"] = button_clicked
+
+input = tkinter.Entry(width=10)
+input.pack()
+
 window.mainloop()
