1cc1b2b92c752a7ad6b9e7a16d8e73214239cf84
kswkev <kswkev@hotmail.com>
2024-02-10 15:17:37 +0000

    Day 35 msmService updated to use environment vars

--- day_35_rain_alert/smsService.py
+++ day_35_rain_alert/smsService.py
@@ -1,15 +1,15 @@
 #twilio recovery code = M8H7GQK8BZW5T3K2QAL215WD
 from twilio.rest import Client
-
-SENDING_PHONE_NUMBER = "+447488897864"
-SID = "ACc27108cebc24480527cd5b424f44c49a"
-AUTH_TOKEN = "b90511728fbb0f329851e39ebdba96a5"
+import os
 
 class SmsService:
 
     def __init__(self):
-        self.client = Client(SID, AUTH_TOKEN)
+        self.SENDING_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+        self.SID = os.getenv("TWILIO_SID")
+        self.AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+        self.client = Client(self.SID, self.AUTH_TOKEN)
 
     def send_message(self, recipient_number, message):
-        message = self.client.messages.create(from_=SENDING_PHONE_NUMBER, to=recipient_number, body=message)
+        message = self.client.messages.create(from_=self.SENDING_PHONE_NUMBER, to=recipient_number, body=message)
         print(message.status)
